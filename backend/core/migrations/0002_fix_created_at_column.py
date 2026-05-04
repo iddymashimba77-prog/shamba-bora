d24151b62a5ff9c0931b14f25565ef5ca85b56bb
Iddy mashimba <iddymashimba77@gmail.com>
2026-05-05 00:34:05 +0300

    Fix created_at typo in database column

---
+++ backend/core/migrations/0002_fix_created_at_column.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+class Migration(migrations.Migration):
+dependencies = [
+('core', '0001_initial'),
+]
+operations = [
+migrations.RunSQL(
+"ALTER TABLE core_farm RENAME COLUMN created_at TO created_at;", 
+reverse_sql="ALTER TABLE core_farm RENAME COLUMN created_at TO created_at;"
+),
+]
