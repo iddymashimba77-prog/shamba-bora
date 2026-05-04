6b385443a629e3bc57c92f856f1a825bdcb17dca
Iddy mashimba <iddymashimba77@gmail.com>
2026-05-05 02:28:36 +0300

    Add latitude lomgitude via build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.db import connection
+cursor = connection.cursor()
+cursor.execute('ALTER TABLE core_farm ADD COLUMN IF NOT EXISTS latitude DECIMAL(9,6) NULL;')
+cursor.execute('ALTER TABLE core_farm ADD COLUMN IF NOT EXISTS longitude DECIMAL(9,6) NULL;')
+print('Columns added successfully!')
+"
