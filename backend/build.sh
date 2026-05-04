5fac6d7e41942112244eb30080237b314fe800f1
Iddy mashimba <iddymashimba77@gmail.com>
2026-05-04 10:50:39 +0300

    Fix migration with fake-initial

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-set -o errexit
-
+set -0 erresxit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --fake-initial
