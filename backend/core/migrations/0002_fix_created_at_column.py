193d8f0b7ef36e2c93b757aeb3d15775d477c9e5
Iddy mashimba <iddymashimba77@gmail.com>
2026-05-05 01:23:30 +0300

    Add latitude and longitude columns

--- backend/core/migrations/0002_fix_created_at_column.py
+++ backend/core/migrations/0002_fix_created_at_column.py
@@ -1,15 +1,19 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),
+        ('core', '0002_fix_created_at_column'),
     ]
 
     operations = [
         migrations.RunSQL(
-            "ALTER TABLE core_farm RENAME COLUMN craeted_at TO created_at;",
-            reverse_sql="ALTER TABLE core_farm RENAME COLUMN created_at TO craeted_at;"
+            "ALTER TABLE core_farm ADD COLUMN IF NOT EXISTS latitude DECIMAL(9,6) NULL;",
+            reverse_sql="ALTER TABLE core_farm DROP COLUMN IF EXISTS latitude;"
+        ),
+        migrations.RunSQL(
+            "ALTER TABLE core_farm ADD COLUMN IF NOT EXISTS longitude DECIMAL(9,6) NULL;",
+            reverse_sql="ALTER TABLE core_farm DROP COLUMN IF EXISTS longitude;"
         ),
     ]
