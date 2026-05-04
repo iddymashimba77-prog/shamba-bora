5310adc45dcf520aa4ab4dc1fcab271885994d2d
Iddy mashimba <iddymashimba77@gmail.com>
2026-05-05 01:15:07 +0300

    Fix migration indentation

--- backend/core/migrations/0002_fix_created_at_column.py
+++ backend/core/migrations/0002_fix_created_at_column.py
@@ -1,11 +1,15 @@
 from django.db import migrations
+
+
 class Migration(migrations.Migration):
-dependencies = [
-('core', '0001_initial'),
-]
-operations = [
-migrations.RunSQL(
-"ALTER TABLE core_farm RENAME COLUMN created_at TO created_at;", 
-reverse_sql="ALTER TABLE core_farm RENAME COLUMN created_at TO created_at;"
-),
-]
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            "ALTER TABLE core_farm RENAME COLUMN craeted_at TO created_at;",
+            reverse_sql="ALTER TABLE core_farm RENAME COLUMN created_at TO craeted_at;"
+        ),
+    ]
