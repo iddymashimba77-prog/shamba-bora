f53e1753f2aacad829a7a3b4d6ea2bab2c489df0
Iddy mashimba <iddymashimba77@gmail.com>
2026-05-05 01:44:43 +0300

    Fix build.sh typo

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -0 erresxit
+set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --fake-initial
+python manage.py migrate
